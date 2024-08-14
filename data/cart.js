d30d6820b0422fcd254a1da718b0e0aa72038d14
Aman7902k <amankhan9034820992@gmail.com>
2024-08-14 14:47:01 +0530

    delete button added

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,49 @@
-const cart = []
+export let cart = JSON.parse(localStorage.getItem('cart'))
+
+if(!cart){
+    cart = [{
+        product_Id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity:2
+    },
+    {
+        product_Id:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity:4
+    }]
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart))   
+}
+
+export function removeItem(productId){
+    let newCart = []
+    cart.forEach((item)=>{
+        if(item.product_Id !== productId){
+            newCart.push(item)
+        }
+    })
+    cart = newCart
+    saveToStorage()
+}
+
+export function addToCart(product_id){
+    let matchingItem;
+    let Quantity = parseInt(document.querySelector(`.quantity_container_${product_id}`).value)
+
+    cart.forEach((item)=>{
+      if(product_id === item.product_Id){
+        matchingItem = item
+      }
+    })
+
+    if(matchingItem){
+      matchingItem.quantity +=Quantity
+    }
+    else{
+      cart.push({
+        product_Id:product_id,
+        quantity:Quantity
+      })
+    }  
+    saveToStorage()
+}
